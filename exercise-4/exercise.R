d41e9e28fc399adef7da58c54dcdd60b7162f99e
EmmanuelRobi <emanr@uw.edu>
2017-10-20 16:36:11 -0700

    finished exercises

--- exercise-4/exercise.R
+++ exercise-4/exercise.R
@@ -7,27 +7,37 @@ library(dplyr)
 
 # The data.frame flights should now be accessible to you.  View it, 
 # and get some basic information about the number of rows/columns
-
+View(flights)
+dim(flights)
+nrow(flights)
+ncol(flights)
+?flights
 
 # Add a column that is the amount of time gained in the air (`arr_delay` - `dep_delay`)
-
+flights <- mutate(flights, gain = arr_delay - dep_delay)
 
 # Sort your data.frame desceding by the column you just created
-
+flights <- arrange(flights, desc(gain))
 
 # Try doing the last 2 steps in a single operation using the pipe operator
-
+flights <- flights %>% mutate(gain = arr_delay - dep_delay) %>% arrange(desc(gain))
 
 # Make a histogram of the amount of gain using the `hist` command
-
+hist(flights$gain)
 
 # On average, did flights gain or lose time?
-
+mean(flights$gain, na.rm = TRUE) ## Lost 5 minutes!
 
 # Create a data.frame that is of flights headed to seatac ('SEA'), 
-
+to.sea <- flights %>% select(gain, dest) %>% filter(dest == 'SEA') 
 
 # On average, did flights to seatac gain or loose time?
+mean(to.sea$gain, na.rm = TRUE) ## Lost 11 minutes!
+
+OriginDestInterest <- function(my_origin, my_dest, interest) {
+  ret <- flights %>% filter(origin == my_origin, dest == my_dest) %>% select_(interest)
+  return(ret)
+}
 
 ### Bonus ###
 # Write a function that allows you to specify an origin, a destination, and a column of interest
@@ -36,9 +46,13 @@ library(dplyr)
 
 
 # Retireve the air_time column for flights from JFK to SEA
+jfk.to.sea <- OriginDestInterest('JFK', 'SEA', 'air_time')
 
 
 # What was the average air time of those flights (in hours)?  
 
 
 # What was the min/max average air time for the JFK to SEA flights?
+mean(jfk.to.sea$air_time, na.rm = TRUE)/60
+min(jfk.to.sea$air_time, na.rm = TRUE)/60
+max(jfk.to.sea$air_time, na.rm = TRUE)/60
